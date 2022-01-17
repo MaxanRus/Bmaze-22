0d997d794f6029174f596436d0124e3b5f234c53
MaxanRus <timosha1113@yandex.ru>
2022-01-17 05:44:57 +0300

    add start server

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,95 @@
 #include <iostream>
+#include <thread>
 
 #include "engine/game_controller.hpp"
 #include "engine/map_generator.hpp"
 
 #include "network/acceptor.hpp"
 
+void StartServer() {
+  using namespace boost::asio;
+  using namespace network;
+  io_service io_service;
+  ip::tcp::endpoint ep(ip::address::from_string("127.0.0.1"), 7777);
+  std::cout << "Server start" << std::endl;
+
+  Acceptor acceptor(io_service, ep, std::make_shared<WaitingLobby>());
+  io_service.run();
+}
+
 int main() {
   using namespace utils;
   using namespace engine;
   using namespace events;
+  using namespace network;
 
-  size_t seed = time(0);
-  std::cout << "current seed: " << seed << std::endl; 
-  GameController gc(MapGenerator(1, 5, 5, seed)());
+  try {
+    std::thread server(StartServer);
 
-  auto print = [](const auto& lst) {
-    for (auto& i: lst) {
-      std::cout << i->GetMe() << ", ";
-    }
-    std::cout << std::endl;
-  };
+    size_t seed = time(0);
+    std::cout << "current seed: " << seed << std::endl; 
+    GameController gc(MapGenerator(1, 5, 5, seed)());
 
-  while (true) {
-    char type;
-    std::cin >> type;
-
-    engine::events::ListEvents list_events;
-    if (type == 'm') {
-      char direction;
-      std::cin >> direction;
-
-      Direction dir;
-      if (direction == 'l') dir = Direction::LEFT;
-      else if (direction == 'u') dir = Direction::UP;
-      else if (direction == 'r') dir = Direction::RIGHT;
-      else dir = Direction::DOWN;
-      list_events = gc.Step(std::make_unique<MoveCurrentPlayer>(dir));
-      print(list_events);
-    } else if (type == 'b') {
-      char direction;
-      std::cin >> direction;
-
-      Direction dir;
-      if (direction == 'l') dir = Direction::LEFT;
-      else if (direction == 'u') dir = Direction::UP;
-      else if (direction == 'r') dir = Direction::RIGHT;
-      else dir = Direction::DOWN;
-      list_events = gc.Step(std::make_unique<PlayerBuildWall>(dir));
-      print(list_events);
-    }
-
-    for (auto& i: list_events) {
-      if (i->event_type == engine::events::EventType::PlayerTriesRaiseExcessTreasure) {
-        auto& event = dynamic_cast<engine::events::PlayerTriesRaiseExcessTreasure&>(*i.get());
-        std::cout << "You must to change treasure(print number) or 0 for to opt out";
+    auto print = [](const auto& lst) {
+      for (auto& i: lst) {
+        std::cout << i->GetMe() << ", ";
       }
-    }
+      std::cout << std::endl;
+    };
 
-    std::cout << std::to_string(gc.GetMap()) << std::endl;
+    bool end_game = false;
+
+    while (!end_game) {
+      char type;
+      if (!(std::cin >> type)) {
+        end_game = true;
+        break;
+      }
+
+      engine::events::ListEvents list_events;
+      if (type == 'm') {
+        char direction;
+        if (!(std::cin >> direction)) {
+          end_game = true;
+          break;
+        }
+
+        Direction dir;
+        if (direction == 'l') dir = Direction::LEFT;
+        else if (direction == 'u') dir = Direction::UP;
+        else if (direction == 'r') dir = Direction::RIGHT;
+        else dir = Direction::DOWN;
+        list_events = gc.Step(std::make_unique<MoveCurrentPlayer>(dir));
+        print(list_events);
+      } else if (type == 'b') {
+        char direction;
+        std::cin >> direction;
+
+        Direction dir;
+        if (direction == 'l') dir = Direction::LEFT;
+        else if (direction == 'u') dir = Direction::UP;
+        else if (direction == 'r') dir = Direction::RIGHT;
+        else dir = Direction::DOWN;
+        list_events = gc.Step(std::make_unique<PlayerBuildWall>(dir));
+        print(list_events);
+      } else if (type == 'd') {
+        end_game = false;
+        break;
+      }
+
+      for (auto& i: list_events) {
+        if (i->event_type == engine::events::EventType::PlayerTriesRaiseExcessTreasure) {
+          auto& event = dynamic_cast<engine::events::PlayerTriesRaiseExcessTreasure&>(*i.get());
+          std::cout << "You must to change treasure(print number) or 0 for to opt out";
+        }
+      }
+
+      std::cout << std::to_string(gc.GetMap()) << std::endl;
+    }
+    server.detach();
+  }
+  catch(...) {
+
   }
 }
 
